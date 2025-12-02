fdda9986c6acd8a8a26e9cf9e730a898c436cd8f
lalabinbin <binhminh2711@gmail.com>
2025-12-02 20:07:34 +0700

    update server URL in Swagger configuration to use environment variable

--- src/swagger.js
+++ src/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3001/api",
+        url: process.env.API_URL,
         description: "Local server",
       },
     ],
